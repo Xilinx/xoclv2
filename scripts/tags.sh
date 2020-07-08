9aac3f1c36be8bfcd4c9801f7d58a46b7c57813a
Sonal Santan <sonal.santan@xilinx.com>
2020-07-08 12:53:43 -0700

    Refactor directory structure and add infrastructure files (#1)
    * Refactor directory structure and add infrastructure files
    
    * Add top level Makefile and TAGS generation

---
+++ scripts/tags.sh
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (C) 2019 Xilinx, Inc. All rights reserved.
+#
+# Authors: Sonal.Santan@xilinx.com
+
+#!/bin/bash
+
+set -e
+
+ROOT=`git rev-parse --show-toplevel`
+ROOTSRC="$ROOT/src"
+out=TAGS
+
+echo "Generating Emacs TAGS file $out from $ROOTSRC ..."
+ALL_FILES=$(git ls-files --exclude-standard --full-name $ROOTSRC)
+for item in $ALL_FILES;
+do
+    FILES=("${FILES[@]}$ROOT/$item\n")
+done
+echo -e ${FILES[@]} | ctags --totals -e -L - -f $out
