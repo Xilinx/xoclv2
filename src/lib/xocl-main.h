dc7bc50c5aed4b934b476430ec114be0cbea342c
Max Zhen <maxz@xilinx.com>
2020-07-23 23:29:16 -0700

    fix event support issue

---
+++ src/lib/xocl-main.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Xilinx, Inc.
+ *
+ * Authors:
+ *	Cheng Zhen <maxz@xilinx.com>
+ */
+
+#ifndef	_XOCL_MAIN_H_
+#define	_XOCL_MAIN_H_
+
+extern struct platform_driver xocl_partition_driver;
+extern struct platform_driver xocl_test_driver;
+
+extern const char *xocl_drv_name(enum xocl_subdev_id id);
+extern int xocl_drv_get_instance(enum xocl_subdev_id id, int instance);
+extern void xocl_drv_put_instance(enum xocl_subdev_id id, int instance);
+
+#endif	/* _XOCL_PARTITION_H_ */
