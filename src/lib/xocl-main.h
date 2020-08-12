57c10775429e89a9c1d9a9765e00054be96271f3
Max Zhen <40219623+maxzhen@users.noreply.github.com>
2020-08-12 10:53:54 -0700

    add mailbox leaf driver (#36)

--- src/lib/xocl-main.h
+++ src/lib/xocl-main.h
@@ -15,12 +15,14 @@ extern struct platform_driver xocl_vsec_driver;
 extern struct platform_driver xocl_vsec_golden_driver;
 extern struct platform_driver xocl_axigate_driver;
 extern struct platform_driver xocl_qspi_driver;
+extern struct platform_driver xocl_mailbox_driver;
 
 extern struct xocl_subdev_endpoints xocl_vsec_endpoints[];
 extern struct xocl_subdev_endpoints xocl_vsec_golden_endpoints[];
 extern struct xocl_subdev_endpoints xocl_axigate_endpoints[];
 extern struct xocl_subdev_endpoints xocl_test_endpoints[];
 extern struct xocl_subdev_endpoints xocl_qspi_endpoints[];
+extern struct xocl_subdev_endpoints xocl_mailbox_endpoints[];
 
 extern const char *xocl_drv_name(enum xocl_subdev_id id);
 extern int xocl_drv_get_instance(enum xocl_subdev_id id);
