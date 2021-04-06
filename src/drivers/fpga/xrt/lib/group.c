8a0d768fa081ec8d75ebaed9b0c0d866ccb7a6b0
Max Zhen <maxz@xilinx.com>
2021-04-06 14:32:20 -0700

    address Tom's comments for 05/20

--- src/drivers/fpga/xrt/lib/group.c
+++ src/drivers/fpga/xrt/lib/group.c
@@ -117,6 +117,11 @@ static int xrt_grp_create_leaves(struct xrt_group *xg)
 	mutex_lock(&xg->lock);
 
 	if (xg->leaves_created) {
+		/*
+		 * This is expected since caller does not keep track of the state of the group
+		 * and may, in some cases, still try to create leaves after it has already been
+		 * created. This special error code will let the caller know what is going on.
+		 */
 		mutex_unlock(&xg->lock);
 		return -EEXIST;
 	}
@@ -153,6 +158,11 @@ static int xrt_grp_create_leaves(struct xrt_group *xg)
 			/* Found a dtb for this instance, let's add it. */
 			ret = xrt_subdev_pool_add(&xg->leaves, did, xrt_grp_root_cb, xg, dtb);
 			if (ret < 0) {
+				/*
+				 * It is not a fatal error here. Some functionality is not usable
+				 * due to this missing device, but the error can be handled
+				 * when the functionality is used.
+				 */
 				failed++;
 				xrt_err(xg->xdev, "failed to add %s: %d", xrt_drv_name(did), ret);
 			}
